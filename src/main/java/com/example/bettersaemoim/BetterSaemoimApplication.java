fe4bbff9082088a753f77a5ee23b18a5881d9255
leejiseop <lkj0245@naver.com>
2024-03-17 18:25:32 +0900

    new spring project ver 3.2.3

---
+++ src/main/java/com/example/bettersaemoim/BetterSaemoimApplication.java
@@ -0,0 +1,13 @@
+package com.example.bettersaemoim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterSaemoimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterSaemoimApplication.class, args);
+	}
+
+}
